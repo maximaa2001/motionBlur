f7f28e9875060a5a60dc94f77ae781a3b726aa02
maks <maximaa2001@gmail.com>
2023-01-20 18:40:55 +0300

    changes: - modified Main - added images

--- src/main/java/by/bsuir/Main.java
+++ src/main/java/by/bsuir/Main.java
@@ -4,9 +4,11 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        String pathToImage = System.getProperty("user.dir").concat("/flowers.jpg");
+        String pathToMotionImage = System.getProperty("user.dir").concat("/motionFlowers.jpg");
         Kernel kernel = new Kernel(5);
-        Image image = new Image("/home/maks/a.jpg");
-        BlurService blurService = new BlurService(image, kernel, "a.jpeg");
+        Image image = new Image(pathToImage);
+        BlurService blurService = new BlurService(image, kernel, pathToMotionImage);
         blurService.imposeKernel();
     }
 }
